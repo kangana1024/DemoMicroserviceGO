fd4b3ae93e11832f6e038eac914d05bd22180a92
Theerakrit Prommoon <theerakrit@isaactech.co>
2019-07-21 14:01:19 +0700

    git init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Use Nginx Reverse Proxy to serve Go+Postgress Services Demo
+Golang served by Nginx reverse proxy.
+
+#  <font color='red'>Code Building</font>
+
+- `docker-compose down`
+- `docker-compose up --build -d`
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", DoHealthCheck).Methods("GET")
+	router.HandleFunc("/api/", DoHealthCheckApi).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
+func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, i'm a golang microservice")
+}
+func DoHealthCheckApi(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, i'm a golang microservice API")
+}
